01349177542d339263ba765b1efc7c8e71e4dec2
energybubu <ivanhoward011878@gmail.com>
2023-10-03 11:58:40 +0800

    backend_url

--- src/pages/constant.jsx
+++ src/pages/constant.jsx
@@ -1,3 +1,3 @@
 // export const URL = "https://chief-partly-goldfish.ngrok-free.app"
 // export const URL = "http://localhost:6012"
-export const URL = "http://140.112.30.56:6012/"
+export const URL = "http://140.112.30.56:6012"
