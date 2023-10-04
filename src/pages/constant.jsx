a67c5e95771acb2427ab8ac366f2c0c909aee6b4
energybubu <ivanhoward011878@gmail.com>
2023-10-04 11:56:54 +0800

    Backend_url

--- src/pages/constant.jsx
+++ src/pages/constant.jsx
@@ -1,3 +1,4 @@
 // export const URL = "https://chief-partly-goldfish.ngrok-free.app"
-export const URL = "http://127.0.0.1:6012"
+// export const URL = "http://127.0.0.1:6012"
 // export const URL = "http://140.112.30.56:6012"
+export const URL = "https://nlg6.csie.ntu.edu.tw/yclee/ConvLogRecaller"
